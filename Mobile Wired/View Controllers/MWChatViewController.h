mattprice/Mobile-Wired

commit 5880ea02cf40b7e67c14d87bfb1ebf976c9c3272

Transition from NSUserDefaults to NSCoding.

Mobile Wired/View Controllers/MWSettingsViewController.m
@@ -39,8 +39,8 @@
 {
     [super viewDidLoad];
 
-    _nicknameTextField.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"UserNick"];
-    _statusTextField.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"UserStatus"];
+    _nicknameTextField.text = [MWDataStore optionForKey:kMWUserNick];
+    _statusTextField.text = [MWDataStore optionForKey:kMWUserStatus];
 
     [TestFlight passCheckpoint:@"Viewed Settings"];
 }
@@ -54,9 +54,9 @@
 
 - (IBAction)saveButtonPressed:(id)sender
 {
-    [[NSUserDefaults standardUserDefaults] setObject:_nicknameTextField.text forKey:@"UserNick"];
-    [[NSUserDefaults standardUserDefaults] setObject:_statusTextField.text forKey:@"UserStatus"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [MWDataStore setOption:_nicknameTextField.text forKey:kMWUserNick];
+    [MWDataStore setOption:_statusTextField.text forKey:kMWUserStatus];
+    [MWDataStore save];
 
     [TestFlight passCheckpoint:@"Modified Settings"];
 

Mobile Wired.xcodeproj/project.pbxproj
@@ -21,6 +21,7 @@
 		0D640C8B194769120069F108 /* UIImage+MWKit.m in Sources */ = {isa = PBXBuildFile; fileRef = 0D640C88194769120069F108 /* UIImage+MWKit.m */; };
 		0D6B66261606475E002F5710 /* WiredSpec_2.0b53.xml in Resources */ = {isa = PBXBuildFile; fileRef = 0D6B66251606475E002F5710 /* WiredSpec_2.0b53.xml */; };
 		0DA8B04C17277CFE008297ED /* QuartzCore.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 0DA8B04B17277CFE008297ED /* QuartzCore.framework */; };
+		0DCF83E8194CBEB100511AD8 /* MWDataStore.m in Sources */ = {isa = PBXBuildFile; fileRef = 0DCF83E7194CBEB100511AD8 /* MWDataStore.m */; };
 		0DD4C0D715BCD8960030069F /* UserInfoViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = 0DD4C0D515BCD8960030069F /* UserInfoViewController.m */; };
 		0DD4C0DB15BCDA9E0030069F /* UserInfoView.xib in Resources */ = {isa = PBXBuildFile; fileRef = 0DD4C0D915BCDA9E0030069F /* UserInfoView.xib */; };
 		0DFF15091941420000294943 /* MWDrawerController.m in Sources */ = {isa = PBXBuildFile; fileRef = 0DFF15081941420000294943 /* MWDrawerController.m */; };
@@ -68,6 +69,8 @@
 		0D640C88194769120069F108 /* UIImage+MWKit.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = "UIImage+MWKit.m"; path = "Categories/UIImage+MWKit.m"; sourceTree = "<group>"; };
 		0D6B66251606475E002F5710 /* WiredSpec_2.0b53.xml */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xml; name = WiredSpec_2.0b53.xml; path = Resources/WiredSpec_2.0b53.xml; sourceTree = "<group>"; };
 		0DA8B04B17277CFE008297ED /* QuartzCore.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = QuartzCore.framework; path = System/Library/Frameworks/QuartzCore.framework; sourceTree = SDKROOT; };
+		0DCF83E6194CBEB100511AD8 /* MWDataStore.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = MWDataStore.h; sourceTree = "<group>"; };
+		0DCF83E7194CBEB100511AD8 /* MWDataStore.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = MWDataStore.m; sourceTree = "<group>"; };
 		0DD4C0D415BCD8960030069F /* UserInfoViewController.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = UserInfoViewController.h; path = Classes/UserInfoViewController.h; sourceTree = "<group>"; };
 		0DD4C0D515BCD8960030069F /* UserInfoViewController.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = UserInfoViewController.m; path = Classes/UserInfoViewController.m; sourceTree = "<group>"; };
 		0DD4C0DA15BCDA9E0030069F /* en */ = {isa = PBXFileReference; lastKnownFileType = file.xib; name = en; path = Resources/en.lproj/UserInfoView.xib; sourceTree = "<group>"; };
@@ -248,6 +251,8 @@
 				0DFF150A194147C100294943 /* MWMacros.h */,
 				0D113DB519426B430056E5D3 /* MWAppDelegate.h */,
 				0D113DB619426B430056E5D3 /* MWAppDelegate.m */,
+				0DCF83E6194CBEB100511AD8 /* MWDataStore.h */,
+				0DCF83E7194CBEB100511AD8 /* MWDataStore.m */,
 				0DFF15071941420000294943 /* MWDrawerController.h */,
 				0DFF15081941420000294943 /* MWDrawerController.m */,
 				0D0B50E3180CDF8200198D07 /* MobileWired.storyboard */,
@@ -316,7 +321,7 @@
 			attributes = {
 				CLASSPREFIX = MW;
 				LastUpgradeCheck = 0500;
-				ORGANIZATIONNAME = "Ember Code";
+				ORGANIZATIONNAME = "Matthew Price";
 				TargetAttributes = {
 					34EBE3BD13594F7A00A24C01 = {
 						DevelopmentTeam = DL2J7C6ZBQ;
@@ -416,6 +421,7 @@
 				0DFF15091941420000294943 /* MWDrawerController.m in Sources */,
 				34EBE3D213594F7B00A24C01 /* main.m in Sources */,
 				34EBE42B1359569B00A24C01 /* ChatViewController.m in Sources */,
+				0DCF83E8194CBEB100511AD8 /* MWDataStore.m in Sources */,
 				3477029F135A026200F91A8B /* WiredConnection.m in Sources */,
 				0D640C89194769120069F108 /* NSString+Base64.m in Sources */,
 				0D640C82194767CA0069F108 /* UITableView+Subviews.m in Sources */,

Mobile Wired/Classes/ChatViewController.m
@@ -37,8 +37,7 @@
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
+    if ((self = [super init])) {
         self.message = @"";
         self.userID = @"";
     }
@@ -50,24 +49,26 @@
 
 @implementation ChatViewController
 
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 }
 
 - (void)loadConnectionSettings {
+    // TODO: Need to update all this stuff. Do we still have observers for NSDefaults?
+    //       We need to use MWDataStore delegates instead?
+    
     // NSUserDefaultsDidChangeNotification doesn't let us know what changed so we tell the server
     // about anything that could have possibly updated.
     
-    NSString *nick = bookmark[@"UserNick"];
+    NSString *nick = bookmark[kMWUserNick];
     if ([nick isEqualToString:@""]) {
-        nick = [[NSUserDefaults standardUserDefaults] stringForKey:@"UserNick"];
+        nick = [MWDataStore optionForKey:kMWUserNick];
     }
     
-    NSString *status = bookmark[@"UserStatus"];
+    NSString *status = bookmark[kMWUserStatus];
     if ([status isEqualToString:@""]) {
-        status = [[NSUserDefaults standardUserDefaults] stringForKey:@"UserStatus"];
+        status = [MWDataStore optionForKey:kMWUserStatus];
     }
     
     [self.connection setNick:nick];
@@ -115,7 +116,7 @@
     chatMessages = [NSMutableArray new];
     
     // Connect to the bookmark.
-    bookmark = [[NSUserDefaults standardUserDefaults] valueForKey:@"Bookmarks"][indexRow];
+    bookmark = [MWDataStore bookmarkAtIndex:indexRow];
     [self connect];
 }
 
@@ -139,7 +140,7 @@
     // Create a new WiredConnection.
     self.connection = [[WiredConnection alloc] init];
     self.connection.delegate = self;
-    [self.connection connectToServer:bookmark[@"ServerHost"] onPort:[bookmark[@"ServerPort"] integerValue]];
+    [self.connection connectToServer:bookmark[kMWServerHost] onPort:[bookmark[kMWServerPort] integerValue]];
     self.userListView.connection = self.connection;
 }
 
@@ -391,7 +392,7 @@
     progressHUD.labelText = @"Logging In";
     
     [self loadConnectionSettings];
-    [self.connection sendLogin:bookmark[@"UserLogin"] withPassword:bookmark[@"UserPass"]];
+    [self.connection sendLogin:bookmark[kMWUserLogin] withPassword:bookmark[kMWUserPass]];
 }
 
 /*

Mobile Wired/Classes/WiredConnection.h
@@ -55,7 +55,7 @@
 
 - (id)init;
 
-#pragma mark User Commands
+#pragma mark - User Commands
 - (void)connectToServer:(NSString *)server onPort:(NSInteger)port;
 - (void)disconnect;
 - (void)sendLogin:(NSString *)user withPassword:(NSString *)password;
@@ -64,12 +64,12 @@
 - (void)setIcon:(NSData *)icon;
 - (void)setIdle;
 
-#pragma mark Connection Information
+#pragma mark - Connection Information
 - (NSDictionary *)getMyUserInfo;
 - (NSDictionary *)getMyPermissions;
 - (void)getInfoForUser:(NSString *)userID;
 
-#pragma mark Channel Commands
+#pragma mark - Channel Commands
 - (void)joinChannel:(NSString *)channel;
 - (void)leaveChannel:(NSString *)channel;
 - (void)sendChatMessage:(NSString *)message toChannel:(NSString *)channel;
@@ -80,7 +80,7 @@
 - (void)kickUserID:(NSString *)userID fromChannel:(NSString *)channel message:(NSString *)message;
 - (void)banUserID:(NSString *)userID message:(NSString *)message expiration:(NSDate *)expiration;
 
-#pragma mark Connection Helpers
+#pragma mark - Connection Helpers
 - (void)sendCompatibilityCheck;
 - (void)sendClientInformation;
 - (void)sendAcknowledgement;
@@ -90,7 +90,7 @@
 - (void)readData;
 - (void)attemptReconnection;
 
-#pragma mark GCDAsyncSocket Wrappers
+#pragma mark - GCDAsyncSocket Wrappers
 - (void)secureSocket;
 - (void)sendTransaction:(NSString *)transaction withParameters:(NSDictionary *)parameters;
 - (void)sendTransaction:(NSString *)transaction;
@@ -98,6 +98,7 @@
 
 @end
 
+#pragma mark - Delegate Methods
 @protocol WiredConnectionDelegate <NSObject>
 - (void)didReceiveServerInfo:(NSDictionary *)serverInfo;
 - (void)didReceiveUserInfo:(NSDictionary *)info;

Mobile Wired/Classes/WiredConnection.m
@@ -51,7 +51,7 @@
     return self;
 }
 
-#pragma mark User Commands
+#pragma mark - User Commands
 /*
  * Connects to the given server and port specified.
  *
@@ -161,7 +161,7 @@
     [self readData];
 }
 
-#pragma mark Connection Info
+#pragma mark - Connection Info
 
 - (NSDictionary *)getMyUserInfo
 {
@@ -182,7 +182,7 @@
     [self readData];
 }
 
-#pragma mark Channel Commands
+#pragma mark - Channel Commands
 
 /*
  * Attempts to join a channel on the Wired server.
@@ -366,7 +366,7 @@
     [self readData];
 }
 
-#pragma mark Connection Helpers
+#pragma mark - Connection Helpers
 
 /*
  * Sends a compatibility check to the server.
@@ -489,7 +489,7 @@
     [self connectToServer:serverHost onPort:serverPort];
 }
 
-#pragma mark GCDAsyncSocket Wrappers
+#pragma mark - GCDAsyncSocket Wrappers
 /*
  * Runs anything that needs to be done post-connection.
  *

Mobile Wired/MWAppDelegate.m
@@ -25,10 +25,6 @@
 
 #import "MWAppDelegate.h"
 
-#import "MMDrawerController.h"
-#import "ChatViewController.h"
-#import "UserListViewController.h"
-#import "MWBookmarksViewController.h"
 #import "TestFlightTokens.h"
 
 @implementation MWAppDelegate
@@ -42,20 +38,8 @@
         [TestFlight takeOff:TF_APP_TOKEN];
     #endif
 #endif
-    
-    // Set up user defaults if they haven't run the app before.
-    if (![[NSUserDefaults standardUserDefaults] boolForKey:@"HasLaunchedBefore"])
-    {
-        NSLog(@"Setting up user defaults for the first time.");
 
-        NSString *defaultStatus = [NSString stringWithFormat:@"On my %@", [[UIDevice currentDevice] model]];
-
-        [[NSUserDefaults standardUserDefaults] setObject:@"Mobile Wired User" forKey:@"UserNick"];
-        [[NSUserDefaults standardUserDefaults] setObject:defaultStatus forKey:@"UserStatus"];
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"HasLaunchedBefore"];
-        [[NSUserDefaults standardUserDefaults] setObject:@[] forKey:@"Bookmarks"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+    [MWDataStore new];
     
 #if !TARGET_IPHONE_SIMULATOR
     // Register for remote notifications, but only if we're not in the Simulator.

Mobile Wired/MWDataStore.h
@@ -1,0 +1,55 @@
+//
+//  MWDataStore.h
+//  Mobile Wired
+//
+//  Copyright (c) 2014 Matthew Price, http://mattprice.me/
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+#define kMWServerName           @"ServerName"
+#define kMWServerHost           @"ServerHost"
+#define kMWServerPort           @"ServerPort"
+#define kMWUserLogin            @"UserLogin"
+#define kMWUserPass             @"UserPass"
+#define kMWUserNick             @"UserNick"
+#define kMWUserStatus           @"UserStatus"
+
+#define kMWNotifications        @"Notifications"
+#define kMWOnMention            @"OnMention"
+
+@interface MWDataStore : NSObject <NSCoding>
+
++ (void)load;
++ (BOOL)save;
+
+#pragma mark - Bookmarks
++ (void)addBookmark:(NSMutableDictionary *)newBookmark;
++ (void)setBookmark:(NSMutableDictionary *)newBookmark forIndex:(NSUInteger)index;
++ (void)removeBookmarkAtIndex:(NSUInteger)index;
++ (NSDictionary *)bookmarkAtIndex:(NSUInteger)index;
++ (NSMutableArray *)bookmarks;
+
+#pragma mark - Global Settings
++ (id)optionForKey:(NSString *)key;
++ (void)setOption:(id)option forKey:(NSString *)key;
+
+@end

Mobile Wired/MWDataStore.m
@@ -1,0 +1,215 @@
+//
+//  MWDataStore.m
+//  Mobile Wired
+//
+//  Copyright (c) 2014 Matthew Price, http://mattprice.me/
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "MWDataStore.h"
+
+#import "SSKeychain.h"
+
+@implementation MWDataStore
+
+static MWDataStore *sharedInstance;
+
+static NSUInteger version;
+static NSMutableDictionary *settings;
+static NSMutableArray *bookmarks;
+
++ (void)load
+{
+    sharedInstance = [NSKeyedUnarchiver unarchiveObjectWithFile:[MWDataStore dataPath]];
+}
+
++ (BOOL)save
+{
+    return [NSKeyedArchiver archiveRootObject:sharedInstance toFile:[MWDataStore dataPath]];
+}
+
++ (NSString *)dataPath
+{
+    static NSString *dataPath = @"";
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentDirectory = [paths objectAtIndex:0];
+        dataPath = [documentDirectory stringByAppendingPathComponent:@"data.plist"];
+    });
+
+    return dataPath;
+}
+
+#pragma mark - NSCoding Protocol
+
+- (id)initWithCoder:(NSCoder *)coder
+{
+    version = (coder) ? [coder decodeIntegerForKey:@"version"] : 0;
+
+    // New User
+    if (version == 0) {
+        NSLog(@"Creating MWDataStore for the first time.");
+        version = 1;
+
+        settings = [NSMutableDictionary new];
+        settings[kMWUserNick] = @"Mobile Wired User";
+        settings[kMWUserStatus] = [NSString stringWithFormat:@"On my %@", [[UIDevice currentDevice] model]];
+
+        bookmarks = [NSMutableArray new];
+
+        return self;
+    }
+
+    // Future upgrade routines...
+//    if (version < 2) {
+//        NSLog(@"Updating MWDataStore from version %ld to 2.", (long)version);
+//        version = 2;
+//        return self;
+//    }
+
+    // Existing User
+    settings = [coder decodeObjectForKey:@"settings"];
+    bookmarks = [coder decodeObjectForKey:@"bookmarks"];
+
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [coder encodeInteger:version forKey:@"version"];
+    [coder encodeObject:settings forKey:@"settings"];
+    [coder encodeObject:bookmarks forKey:@"bookmarks"];
+}
+
+#pragma mark - Bookmarks
+
++ (NSString *)keychainAccountForBookmark:(NSDictionary *)bookmark
+{
+    NSString *user = bookmark[kMWUserLogin];
+    NSString *host = bookmark[kMWServerHost];
+    NSString *port = bookmark[kMWServerPort];
+
+    return [NSString stringWithFormat:@"%@@%@:%@", user, host, port];
+}
+
++ (void)deleteKeychainAccount:(NSString *)account
+{
+    BOOL success = [SSKeychain deletePasswordForService:[[NSBundle mainBundle] bundleIdentifier] account:account];
+    if (!success) {
+        NSLog(@"Deleting Keychain account '%@' failed.", account);
+    }
+}
+
++ (void)addBookmark:(NSMutableDictionary *)bookmark
+{
+    // Add the password to the Keychain.
+    if (bookmark[kMWUserLogin] && bookmark[kMWUserPass]) {
+        NSLog(@"Adding password to Keychain.");
+
+        [SSKeychain setPassword:bookmark[kMWUserPass]
+                     forService:[[NSBundle mainBundle] bundleIdentifier]
+                        account:[self keychainAccountForBookmark:bookmark]];
+    }
+
+    // Remove the password from the bookmark.
+    [bookmark removeObjectForKey:kMWUserPass];
+
+    // Store the resulting bookmark.
+    [bookmarks addObject:(NSDictionary *)bookmark];
+    [self save];
+
+    [TestFlight passCheckpoint:@"Added Bookmark"];
+}
+
++ (void)setBookmark:(NSMutableDictionary *)bookmark forIndex:(NSUInteger)index
+{
+    // Delete the old password from the Keychain.
+    NSString *account = [self keychainAccountForBookmark:bookmarks[index]];
+    [self deleteKeychainAccount:account];
+
+    // Add the new password to the Keychain.
+    if (bookmark[kMWUserLogin] && bookmark[kMWUserPass]) {
+        NSLog(@"Adding password to Keychain.");
+        [SSKeychain setPassword:bookmark[kMWUserPass]
+                     forService:[[NSBundle mainBundle] bundleIdentifier]
+                        account:[self keychainAccountForBookmark:bookmark]];
+    }
+
+    // Remove the password from the bookmark.
+    [bookmark removeObjectForKey:kMWUserPass];
+
+    // Store the resulting bookmark.
+    [bookmarks setObject:(NSDictionary *)bookmark atIndexedSubscript:index];
+    [self save];
+
+    [TestFlight passCheckpoint:@"Edited Bookmark"];
+}
+
++ (void)removeBookmarkAtIndex:(NSUInteger)index
+{
+    // Delete account from the Keychain.
+    NSString *account = [self keychainAccountForBookmark:bookmarks[index]];
+    [self deleteKeychainAccount:account];
+
+    [bookmarks removeObjectAtIndex:index];
+    [self save];
+}
+
++ (NSDictionary *)bookmarkAtIndex:(NSUInteger)index
+{
+    NSMutableDictionary *bookmark = [bookmarks[index] mutableCopy];
+
+    // Fetch the password from the Keychain.
+    NSString *password = [SSKeychain passwordForService:[[NSBundle mainBundle] bundleIdentifier]
+                                                account:[self keychainAccountForBookmark:bookmark]];
+    if (password) {
+        bookmark[kMWUserPass] = password;
+    }
+
+    return bookmark;
+}
+
++ (NSMutableArray *)bookmarks
+{
+    NSLog(@"Passwords: %@", [[SSKeychain allAccounts] description]);
+
+    NSMutableArray *_bookmarks = [NSMutableArray new];
+    for (int i = 0; i < [bookmarks count]; i++) {
+        _bookmarks[i] = [self bookmarkAtIndex:i];
+    }
+
+    return _bookmarks;
+}
+
+#pragma mark - Global Settings
+
++ (id)optionForKey:(NSString *)key
+{
+    return settings[key];
+}
+
++ (void)setOption:(id)option forKey:(NSString *)key
+{
+    settings[key] = option;
+}
+
+@end

commit f9f1d8c3ef966a37c78099aa599883dae88964eb

Connections are now notified when global settings change.
Fixes a regression introduced in 5880ea0.

Mobile Wired/Resources/en.lproj/LeftDrawer.storyboard
@@ -190,7 +190,7 @@
                         </barButtonItem>
                         <barButtonItem key="rightBarButtonItem" systemItem="save" id="bQP-bO-qme">
                             <connections>
-                                <action selector="saveButtonPressed:" destination="pgP-5g-1Jy" id="sIt-gg-dgO"/>
+                                <segue destination="z0d-rd-1AZ" kind="unwind" unwindAction="settingsSaveButtonPressed:" id="bzJ-v4-N4a"/>
                             </connections>
                         </barButtonItem>
                     </navigationItem>
@@ -200,6 +200,7 @@
                     </connections>
                 </tableViewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="RtF-Mj-7Ie" userLabel="First Responder" sceneMemberID="firstResponder"/>
+                <exit id="z0d-rd-1AZ" userLabel="Exit" sceneMemberID="exit"/>
             </objects>
             <point key="canvasLocation" x="-1101" y="-330"/>
         </scene>
@@ -534,7 +535,7 @@
                         </barButtonItem>
                         <barButtonItem key="rightBarButtonItem" systemItem="save" id="gao-TD-lnI">
                             <connections>
-                                <segue destination="Uql-2y-5NI" kind="unwind" identifier="MWBookmarkSaveSegue" unwindAction="saveButtonPressed:" id="fFt-FJ-7Ge"/>
+                                <segue destination="Uql-2y-5NI" kind="unwind" unwindAction="bookmarkSaveButtonPressed:" id="N2n-0W-49P"/>
                             </connections>
                         </barButtonItem>
                     </navigationItem>

Mobile Wired/View Controllers/MWChatViewController.h
@@ -50,8 +50,10 @@ typedef NS_ENUM(NSInteger, MWChatMessageTypes) {
 @property (strong, nonatomic) WiredConnection *connection;
 @property (strong, nonatomic) MWUserListViewController *userListView;
 
-- (void)loadBookmark:(NSUInteger)indexRow;
 - (Boolean)isConnected;
+- (void)loadBookmark:(NSUInteger)indexRow;
+- (void)sendUserInformation;
+
 - (IBAction)sendButtonPressed:(id)sender;
 - (void)getInfoForUser:(NSString *)userID;
 

Mobile Wired/View Controllers/MWChatViewController.m
@@ -77,29 +77,16 @@
     self.tableView.estimatedRowHeight = 70.0;
 }
 
-- (void)loadConnectionSettings {
-    // TODO: Need to update all this stuff. Do we still have observers for NSDefaults?
-    //       We need to use MWDataStore delegates instead?
-    
-    // NSUserDefaultsDidChangeNotification doesn't let us know what changed so we tell the server
-    // about anything that could have possibly updated.
-    
-    NSString *nick = self.bookmark[kMWUserNick];
-    if ([nick isEqualToString:@""]) {
-        nick = [MWDataStore optionForKey:kMWUserNick];
-    }
-    
-    NSString *status = self.bookmark[kMWUserStatus];
-    if ([status isEqualToString:@""]) {
-        status = [MWDataStore optionForKey:kMWUserStatus];
-    }
-    
-    [self.connection setNick:nick];
-    [self.connection setStatus:status];
-    [self.connection setIcon:nil];
-}
-
 #pragma mark - Wired Connection
+
+- (Boolean)isConnected
+{
+    if (self.connection) {
+        return self.connection.isConnected;
+    } else {
+        return 0;
+    }
+}
 
 - (void)loadBookmark:(NSUInteger)indexRow
 {
@@ -117,15 +104,6 @@
     // Connect to the bookmark.
     self.bookmark = [MWDataStore bookmarkAtIndex:indexRow];
     [self connect];
-}
-
-- (Boolean)isConnected
-{
-    if (self.connection) {
-        return self.connection.isConnected;
-    } else {
-        return 0;
-    }
 }
 
 - (void)connect
@@ -147,6 +125,23 @@
     [self.connection disconnect];
 }
 
+- (void)sendUserInformation {
+    // We don't know what changed so tell the server about anything that could have possibly updated.
+    // Wired Server
+    NSString *nick = self.bookmark[kMWUserNick];
+    if ([nick isEqualToString:@""]) {
+        nick = [MWDataStore optionForKey:kMWUserNick];
+    }
+
+    NSString *status = self.bookmark[kMWUserStatus];
+    if ([status isEqualToString:@""]) {
+        status = [MWDataStore optionForKey:kMWUserStatus];
+    }
+
+    [self.connection setNick:nick];
+    [self.connection setStatus:status];
+    [self.connection setIcon:nil];
+}
 
 #pragma mark - IBActions
 
@@ -437,12 +432,6 @@
     self.userListView.connection = self.connection;
     [self mm_drawerController].rightDrawerViewController = self.userListView;
 
-    // Register to listen for NSUserDefaults changes.
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(loadConnectionSettings)
-                                                 name:NSUserDefaultsDidChangeNotification
-                                               object:nil];
-
     // Register an event for when a keyboard pops up.
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(keyboardWillShow:)
@@ -540,7 +529,7 @@
 
     self.progressHUD.labelText = @"Logging In";
 
-    [self loadConnectionSettings];
+    [self sendUserInformation];
     [self.connection sendLogin:self.bookmark[kMWUserLogin] withPassword:self.bookmark[kMWUserPass]];
 }
 

Mobile Wired/View Controllers/MWServerListViewController.m
@@ -234,10 +234,20 @@ typedef NS_ENUM(NSInteger, MWServerListTableSections) {
     }
 }
 
-- (IBAction)saveButtonPressed:(UIStoryboardSegue *)seque
+- (IBAction)bookmarkSaveButtonPressed:(UIStoryboardSegue *)seque
 {
     self.serverBookmarks = [MWDataStore bookmarks];
     [self.tableView reloadData];
+}
+
+- (IBAction)settingsSaveButtonPressed:(UIStoryboardSegue *)seque
+{
+    // Notify the connections that they should update their settings.
+    [self.currentConnections enumerateKeysAndObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(id key, id obj, BOOL *stop){
+        UINavigationController *controller = (UINavigationController *)obj;
+        MWChatViewController *chatView = controller.viewControllers[0];
+        [chatView sendUserInformation];
+    }];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender

Mobile Wired/View Controllers/MWSettingsViewController.m
@@ -53,7 +53,7 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (IBAction)saveButtonPressed:(id)sender
+- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender
 {
     [MWDataStore setOption:self.nicknameTextField.text forKey:kMWUserNick];
     [MWDataStore setOption:self.statusTextField.text forKey:kMWUserStatus];
@@ -61,7 +61,7 @@
 
     [TestFlight passCheckpoint:@"Modified Settings"];
 
-    [self dismissViewControllerAnimated:YES completion:nil];
+    return YES;
 }
 
 #pragma mark - TextField Delegates